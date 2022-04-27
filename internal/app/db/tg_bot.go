91da4e4b25994adbcaf5766ce11fb135500658f9
sebashmakov <sebashmakov@ozon.ru>
2022-04-28 00:26:30 +0300

    Implemented the method GetUserTelegram

--- internal/app/db/tg_bot.go
+++ internal/app/db/tg_bot.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"errors"
 	sq "github.com/Masterminds/squirrel"
 )
 
@@ -26,14 +27,10 @@ func (r *repository) AddNewUserBot(id int, nameUser string) error {
 			return err
 		}
 
-		rows, errDB := r.db.Query(queryAddNewUser, args...)
+		_, errDB := r.db.Query(queryAddNewUser, args...)
 		if errDB != nil {
 			return errDB
 		}
-		for rows.Next() {
-			return nil
-		}
-
 	}
 	return nil
 }
@@ -64,11 +61,42 @@ func (r *repository) GetUsersTelegram() ([]int, error) {
 	return []int{}, nil
 }
 
+func (r *repository) GetUserTelegram(id int) (*UserDb, error) {
+	queryUserExists, args, err := r.qb.
+		Select("id, name, telegram_id").
+		From("users").
+		Where(sq.Eq{"telegram_id": id}).
+		ToSql()
+
+	if err != nil {
+		return nil, err
+	}
+
+	rows, errDB := r.db.Query(queryUserExists, args...)
+	if errDB != nil {
+		return nil, errDB
+	}
+
+	for rows.Next() {
+		var id, telegramId int
+		var name string
+		rows.Scan(&id, &name, &telegramId)
+
+		return &UserDb{id, name, telegramId}, nil
+	}
+	return nil, errors.New("user does not exist")
+}
+
 func (r *repository) DeleteAdmin(id int) error {
 	return nil
 }
 
 func (r *repository) AddAdmin(id int) error {
-	//r.qb.Insert("")
+
+	//queryAddNewUser, args, err := r.qb.Insert("user_rools").
+	//	Columns("user_id, rool_id").
+	//	Values(nameUser, id).
+	//	ToSql()
+
 	return nil
 }
