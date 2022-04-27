f14b4774bbf1decb9f72df4dcc8b69b795ddf0bf
sebashmakov <sebashmakov@ozon.ru>
2022-04-27 23:22:56 +0300

    Implemented the method UserExists

--- internal/app/db/tg_bot.go
+++ internal/app/db/tg_bot.go
@@ -1,11 +1,38 @@
 package db
 
+import (
+	sq "github.com/Masterminds/squirrel"
+)
+
 func (r *repository) HasAdministratorRools(userTgId int) (bool, error) {
 	return false, nil
 }
 
 func (r *repository) AddNewUserBot(id int, nameUser string) error {
+
 	return nil
+}
+
+func (r *repository) UserExists(userTgID int) (bool, error) {
+	queryUserExists, _, err := r.qb.
+		Select("id").
+		From("users").
+		Where(sq.Eq{"telegram_id": userTgID}).
+		ToSql()
+
+	if err != nil {
+		return false, err
+	}
+
+	rows, errDB := r.db.Query(queryUserExists)
+	if errDB != nil {
+		return false, errDB
+	}
+
+	for rows.Next() {
+		return true, nil
+	}
+	return false, nil
 }
 
 func (r *repository) GetUsersTelegram() ([]int, error) {
