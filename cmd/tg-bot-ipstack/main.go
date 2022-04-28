8d124abea920f9c61313db3ebadf54562547569b
sebashmakov <sebashmakov@ozon.ru>
2022-04-28 21:27:02 +0300

    initializing the bot checking the work

--- cmd/tg-bot-ipstack/main.go
+++ cmd/tg-bot-ipstack/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/Sergei3232/tg-bot-ipstack/internal/app/db"
 	"github.com/Sergei3232/tg-bot-ipstack/internal/config"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"log"
 )
 
@@ -12,5 +14,34 @@ func main() {
 		log.Panicln(err)
 	}
 
-	fmt.Println(configs)
+	db, errDb := db.NewDbConnectClient(configs.DnsDB)
+	if errDb != nil {
+		log.Panic(errDb)
+	}
+
+	bot, err := tgbotapi.NewBotAPI(configs.TokenTelegramBot)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
+	fmt.Println(configs, bot, db)
 }
