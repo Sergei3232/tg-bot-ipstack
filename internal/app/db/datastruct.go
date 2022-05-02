fcf11decedfad5b2c9c5b4b9a6cf116fd1c02a8e
sebashmakov <sebashmakov@ozon.ru>
2022-05-03 00:48:01 +0300

    add datastruct.go UserRequestHistory + UserRequests + HistoryUser

--- internal/app/db/datastruct.go
+++ internal/app/db/datastruct.go
@@ -5,3 +5,17 @@ type UserDb struct {
 	Name       string `json:"name"`
 	TelegramId int    `json:"telegram_id"`
 }
+
+type UserRequestHistory struct {
+	Id          int    `json:"id"`
+	Ip          string `json:"ip"`
+	QueryResult string `json:"query_result"`
+	TimeQuery   string `json:"time_query"`
+}
+
+type UserRequests []UserRequestHistory
+
+type HistoryUser struct {
+	UserDb       `json:"user"`
+	UserRequests `json:"query"`
+}
